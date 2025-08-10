ca6e80f33a009217cb868ab1291e8f0a84d5208d
Adam Ewell <adamantt147@aol.com>
2025-08-10 13:51:37 -0400

    new update for projects

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
-const newPostBtn = document.querySelector(".profile__new-post-btn");
-const newPostModal = document.querySelector(".new-post-modal");
+const newPostBtn = document.querySelector(".profile__add-btn");
+const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
 editProfileBtn.addEventListener("click", function () {
