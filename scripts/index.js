aaa81f06b559ea9a0ac066a715f51f8f2cfdb3e7
Adam Ewell <adamantt147@aol.com>
2025-08-18 09:10:28 -0400

    new update for project

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileform = editProfileModal.querySelector(".modal__form");
+const editProfileSubmitBtn =
+  editProfileModal.querySelector(".modal__submit-btn");
 const editProfileNameInput = editProfileModal.querySelector(
   "#profile-name-input"
 );
@@ -32,3 +35,21 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = editProfileNameInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+  profileDescription.textContent = editProfileDescriptionInput.value;
+}
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+  // Logic to handle new post submission
+  console.log("New post submitted");
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+editProfileform.addEventListener("submit", handleNewPostSubmit);
+
+newPostModal.addEventListener("submit", handleNewPostSubmit);
